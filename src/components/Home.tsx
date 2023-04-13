10beda719da3e5c2b1111d2966965fcc7e7c7c4f
Chad Chapman <chad.chapman2010@gmail.com>
2023-04-13 19:09:21 -0400

    Added shop button and associated styling.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
 import background from "../images/background.png"
+import { Link } from "react-router-dom";
 function Home() {
+
     return (
         <div style={{ backgroundImage: `url(${background})`}}
             className="home">
-            <h1 className="home-elements">Welcome to the Just Cause Shop</h1>
+            <h1 className="home-elements welcome">Welcome to the Just Cause Shop</h1>
             <h2 className="home-elements">The one stop shop for your dictator</h2>
             <h2 className="home-elements">removal specialist equipment needs</h2>
+            <Link className="home-elements" to="shop">
+                <button className="shop-button">Shop</button>
+            </Link>
         </div>
     );
 };
