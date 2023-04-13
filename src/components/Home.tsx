40c5948cfe0988fdf829a1e9c165982189900554
Chad Chapman <chad.chapman2010@gmail.com>
2023-04-13 17:22:04 -0400

    Initial commit.

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import "../App.css";
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Shop.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import "../App.css";
+function Shop() {
+    return (
+        <div>
+            <h1>Shop</h1>
+        </div>
+    );
+};
+
+export default Shop;
