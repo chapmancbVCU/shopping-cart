0f854ddfcfb90bbc33f2d0ad8c2aa0e20cff5d10
Chad Chapman <chad.chapman2010@gmail.com>
2023-04-13 18:26:24 -0400

    Set background image.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
+import background from "../images/background.png"
 function Home() {
     return (
-        <div>
+        <div style={{ backgroundImage: `url(${background})`}}
+            className="home">
             <h1>Home</h1>
         </div>
     );
