9bbcc75e5c0ca9f3d280a14de5139974beffa5a3
Chad Chapman <chad.chapman2010@gmail.com>
2023-04-14 21:07:08 -0400

    Got everything setup to read database.  Successful test.

--- server.ts
+++ server.ts
@@ -8,7 +8,9 @@ app.use(express.json());
 
 // Connect to mongoose
 mongoose.connect("mongodb+srv://chadchapman2010:XAXBXFPc3TdYSqpa@cluster1.yde2tii.mongodb.net/shopping-cart");
+
 //require route
+app.use("/", require("./routes/VehicleRoute"));
 
 app.listen(3001, function() {
     console.log("Express server is running on port 3001");
--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -2,11 +2,27 @@ import React, { useState, useEffect } from "react";
 import "../App.css";
 
 function Shop() {
+    const [vehicle, setVehicle] = useState([{
+        name: '',
+        vehicleType: ''
+    }])
     
-    
+    useEffect(() => {
+        fetch("/vehicles").then(res => {
+            if(res.ok) {
+                return res.json();
+            }
+        }).then(jsonRes => setVehicle(jsonRes));
+    })
     return (
         <div>
             <h1>Shop</h1>
+            {vehicle.map((vehicleInst, index) => 
+                <div key = {index}>
+                    <h3>{vehicleInst.name}</h3>    
+                    <h3>{vehicleInst.vehicleType}</h3>
+                </div>
+            )}
         </div>
     );
 };
